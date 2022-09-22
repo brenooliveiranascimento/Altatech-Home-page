496f9a4bc973698e53599f1a758728a8ae319f9b
Breno Nascimento <satsukebare23@gmail.com>
2022-09-22 13:51:01 -0300

    finish header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -18,37 +18,21 @@ function Header() {
   }, []);
   return (
     <header
-      style={{
-        backgroundColor: scroll > 125 ? 'rgba(28, 32, 62,0.8)' : 'rgba(0,0,0,0.0)',
-      }}
+      style={{ backgroundColor: scroll > 125 ? 'rgba(28, 32, 62,0.8)' : 'rgba(0,0,0,0.0)' }}
       className="header_container"
     >
-      <section
-        style={{
-          height: scroll > 125 ? '7rem' : '10rem',
-        }}
-        className="limit_content"
-      >
+      <section style={{ height: scroll > 125 ? '7rem' : '10rem' }} className="limit_content">
         <section
-          style={{
-            marginBottom: scroll > 125 ? -30 : 0,
-            marginTop: 20,
-          }}
+          style={{ marginBottom: scroll > 125 ? -30 : 0, marginTop: 20 }}
           className="general_top"
         >
           <span className="wathsap">Wathsap</span>
           <FiSearch className="top_icon" />
           <FiShoppingCart className="top_icon" />
         </section>
-        <section style={{
-          display: 'flex', justifyContent: 'space-between',
-        }}
-        >
+        <section style={{ display: 'flex', justifyContent: 'space-between' }}>
           <section className="logo_area">
-            <img
-              src={altaLogo}
-              alt="altatech logo"
-            />
+            <img src={altaLogo} alt="altatech logo" />
             <article>
               <p>Altatech</p>
               <p>Soluções em Tecnologica</p>
@@ -61,15 +45,11 @@ function Header() {
               <span>Locação</span>
               <span>Cloud</span>
               <button
-                style={{
-                  widows: 30,
-                }}
+                style={{ widows: 30 }}
                 type="button"
                 className="buttonMenu"
               >
-                <FiMenu
-                  className="menu"
-                />
+                <FiMenu className="menu" />
               </button>
               <button
                 type="button"
