d3003e88a821e93df88021d19564bc7896b34444
Breno Nascimento <satsukebare23@gmail.com>
2022-09-22 22:14:18 -0300

    add server image

---
+++ src/Components/SideCards/ServerVps/ServerVps.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ServerVps.css';
+
+function ServerVps() {
+  return (
+    <section className="server_container">
+      <section className="server_image_container">
+        <section className="server_image_darkness" />
+      </section>
+    </section>
+  );
+}
+
+export default ServerVps;
