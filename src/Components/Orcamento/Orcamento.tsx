0b32f091fc3d67c762b2164c04356a2f98eb5954
Breno Nascimento <satsukebare23@gmail.com>
2022-09-23 15:31:05 -0300

    componentizando formulario de orçamento

--- src/Components/Orcamento/Orcamento.tsx
+++ src/Components/Orcamento/Orcamento.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Orcamento.css';
 import Lottie from 'react-lottie';
 import formAnimation from '../../lottie/formAnimation.json';
+import OrçamentForm from './OrçamentForm/OrçamentForm';
 
 function Orcamento() {
   const defaultOptionsLoading: any = {
@@ -15,24 +16,16 @@ function Orcamento() {
   return (
     <section className="orcament_container">
       <section className="orcamento_limit">
-        <section className="message_area">
-          <input placeholder="Nome" />
-          <input placeholder="Telefone" />
-          <input placeholder="Email" />
-          <input placeholder="CNPJ" />
-          <textarea placeholder="Menssagem" />
-          <button type="button">
-            Enviar
-          </button>
-        </section>
+        <OrçamentForm />
         <section className="user_form_area">
           <h1>
             Faça o orçamento
           </h1>
           <Lottie
             style={{
-              alignSelf: 'flex-start',
-              justifyContent: 'flex-start',
+              alignSelf: 'center',
+              justifyContent: 'center',
+              marginLeft: -20,
               width: 650,
             }}
             options={defaultOptionsLoading}
