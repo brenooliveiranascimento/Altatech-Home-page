a3d722044b1a05c2fc4717f62018d0bd4d55906f
Breno Nascimento <satsukebare23@gmail.com>
2022-09-23 19:26:30 -0300

    add sidebar

--- src/Components/Partners/Partners.tsx
+++ src/Components/Partners/Partners.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import Eficiencia from '../SideCards/Eficiencia/Eficiencia';
 import './Partners.css';
 
 function Partners() {
   return (
     <section className="partners_container">
-      <section className="partners_limit">
-        <h1>
-          Estamos entre as maiores empresas de serviços em tecnologia
-          do Norte do Paraná. Uma das únicas a cumprir 100% dos projetos entregues.
-        </h1>
-      </section>
+      <Eficiencia />
     </section>
   );
 }
---
+++ src/Components/SideCards/Eficiencia/Eficiencia.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Eficiencia.css';
+
+export default function Eficiencia() {
+  return (
+    <section className="partners_limit">
+      <h1>
+        Estamos entre as maiores empresas de serviços em tecnologia
+        do Norte do Paraná. Uma das únicas a cumprir 100% dos projetos entregues.
+      </h1>
+    </section>
+  );
+}
