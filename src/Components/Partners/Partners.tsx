3c4c9c9b579cbfb2e3ac8616e0b16c986ee18919
Breno Nascimento <satsukebare23@gmail.com>
2022-09-23 19:21:42 -0300

    add side card in partnes

---
+++ src/Components/Partners/Partners.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Partners.css';
+
+function Partners() {
+  return (
+    <section className="partners_container">
+      <section className="partners_limit">
+        <h1>
+          Estamos entre as maiores empresas de serviços em tecnologia
+          do Norte do Paraná. Uma das únicas a cumprir 100% dos projetos entregues.
+        </h1>
+      </section>
+    </section>
+  );
+}
+
+export default Partners;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import Hero from '../../Components/Hero/Hero';
 import Orcamento from '../../Components/Orcamento/Orcamento';
+import Partners from '../../Components/Partners/Partners';
 import Services from '../../Components/Services/Services';
 import CloudService from '../../Components/ServicesInf/CloudService';
 import ServerVps from '../../Components/SideCards/ServerVps/ServerVps';
@@ -15,6 +16,7 @@ export default function Home() {
       <CloudService />
       <ServerVps />
       <Orcamento />
+      <Partners />
     </section>
   );
 }
