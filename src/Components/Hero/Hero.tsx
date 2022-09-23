045cf987788c3fac9e0cba84efc8ff6422f18768
Breno Nascimento <satsukebare23@gmail.com>
2022-09-23 20:41:50 -0300

    add control in hero

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
+import { heroInfType } from '../../types';
 import HeroFooter from '../HeroFooter/HeroFooter';
 import NeedButton from '../NeedButton/NeedButton';
 import './Hero.css';
 
 function Hero() {
+  const heroInf = useSelector(({ interfaceData }: any) => interfaceData.data.hero);
+
   return (
     <section className="hero_container">
       <section className="hero_brightness">
         <section className="hero_inf">
           <article className="left_hero_centent">
             <h1>
-              Altatech Soluções em tecnologia
+              {heroInf}
             </h1>
             <span>Sobre nos</span>
           </article>
