e1c021e7b4cfeadbb8edd5a359f94e31e80e7270
Breno Nascimento <satsukebare23@gmail.com>
2022-09-22 22:38:59 -0300

    finish exterior square

--- src/Components/SideCards/ServerVps/ServerVps.tsx
+++ src/Components/SideCards/ServerVps/ServerVps.tsx
@@ -16,15 +16,18 @@ function ServerVps() {
     <section className="server_container">
       <section className="server_image_container">
         <section className="server_image_darkness">
-          <Lottie
-            style={{
-              alignSelf: 'flex-end',
-              justifyContent: 'center',
-              width: 320,
-              height: 250,
-            }}
-            options={defaultOptionsLoading}
-          />
+          <section className="server_inf_area">
+            <h1>Server VPS: Tenha seu servidor virtual </h1>
+            <Lottie
+              style={{
+                alignSelf: 'flex-end',
+                justifyContent: 'center',
+                width: 320,
+                height: 250,
+              }}
+              options={defaultOptionsLoading}
+            />
+          </section>
           <section className="server_vps_dark_side" />
         </section>
       </section>
