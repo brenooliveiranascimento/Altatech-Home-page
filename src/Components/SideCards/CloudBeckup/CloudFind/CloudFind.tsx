a66a1b6ac631f107930f6f88674cb149245c6d06
Breno Nascimento <satsukebare23@gmail.com>
2022-09-22 21:22:58 -0300

    add minicard

---
+++ src/Components/SideCards/CloudBeckup/CloudFind/CloudFind.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CloudFInd.css';
+
+function CloudFind() {
+  return (
+    <nav className="request_quote">
+      <span>dwaiodw</span>
+    </nav>
+  );
+}
+
+export default CloudFind;
--- src/Components/SideCards/CloudBeckup/CloudInf.tsx
+++ src/Components/SideCards/CloudBeckup/CloudInf.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import CloudFind from './CloudFind/CloudFind';
 import './ServicesInf.css';
 
 function CloudInf() {
   return (
-    <section className="cloud_inf">
-      <h1>Backup em nuvem Corporativo</h1>
-      <p>
-        Uma solução de segurança avançada e econômica para seus dados.
-        <br />
-        <br />
-        Com backups automáticos e criptografados,
-        os dados de sua empresa estarão sempre em segurança no cloud (nuvem) contra desastres,
-        brechas na rede, roubos de equipamentos, ransomware (vírus) ou sequestro virtuais.
-      </p>
+    <section>
+      <CloudFind />
+      <section className="cloud_inf">
+        <h1>Backup em nuvem Corporativo</h1>
+        <p>
+          Uma solução de segurança avançada e econômica para seus dados.
+          <br />
+          <br />
+          Com backups automáticos e criptografados,
+          os dados de sua empresa estarão sempre em segurança no cloud (nuvem) contra desastres,
+          brechas na rede, roubos de equipamentos, ransomware (vírus) ou sequestro virtuais.
+        </p>
+      </section>
     </section>
   );
 }
