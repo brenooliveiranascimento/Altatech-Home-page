6605c5e933af30353423bbcd40ef0021c83713af
Breno Nascimento <satsukebare23@gmail.com>
2022-09-26 21:30:42 -0300

    add form funcionality

--- src/AuthComponents/AuthCard/AuthCard.tsx
+++ src/AuthComponents/AuthCard/AuthCard.tsx
@@ -3,13 +3,41 @@ import './AuthCard.css';
 
 function AuthCard() {
   const [isRegister, setIsRegister] = useState(false);
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    adm: false,
+  });
+
+  const handleUserData = ({ target }: any) => {
+    const { name, value } = target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
 
   return (
     <section className="auth_card_container">
       <h1>Logar</h1>
-      { isRegister && <input name="name" />}
-      <input name="email" />
-      <input name="password" />
+      { isRegister && (
+      <input
+        onChange={handleUserData}
+        value={userData.name}
+        name="name"
+      />
+      )}
+      <input
+        onChange={handleUserData}
+        value={userData.email}
+        name="email"
+      />
+      <input
+        onChange={handleUserData}
+        value={userData.password}
+        name="password"
+      />
       <button type="button">
         { isRegister ? 'Registrar' : 'entrar' }
       </button>
